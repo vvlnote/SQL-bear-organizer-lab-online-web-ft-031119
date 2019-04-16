2d1ce16a4ac595e205870f718efe19592b937d40
Joy Lin <vvlnote@hotmail.com>
2019-04-16 02:25:42 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,7 +30,7 @@ end
 
 def counts_number_of_bears_with_goofy_temperaments
   "Write your SQL query here"
-  
+  "SELECT COUNT(temperament) FROM bears WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
