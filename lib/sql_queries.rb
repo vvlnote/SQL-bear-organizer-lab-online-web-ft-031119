1146d5bc29f176409427ca72de3996c3d8732541
Joy Lin <vvlnote@hotmail.com>
2019-04-16 02:10:31 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,6 +10,7 @@ end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
   "Write your SQL query here"
+  "SELECT name, age ORDER BY age WHERE alive = 1;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
