6accadd760f3044f3a180547b32120d13dff21cc
Joy Lin <vvlnote@hotmail.com>
2019-04-16 02:13:50 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,6 +15,7 @@ end
 
 def selects_oldest_bear_and_returns_name_and_age
   "Write your SQL query here"
+  "SELECT name, age FROM bears ORDER BY age DES"
 end
 
 def select_youngest_bear_and_returns_name_and_age
