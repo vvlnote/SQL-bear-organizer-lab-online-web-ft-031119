dd1f49d096e3f00d5a774df8591e8a7f396d0524
Joy Lin <vvlnote@hotmail.com>
2019-04-16 01:54:06 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO beers (name, age, gender, color, temperament, alive) VALUES ('Mr.Chocolate', 3, 'm', 'dark brown','mild',1)
+INSERT INTO beers (name, age, gender, color, temperament, alive) VALUES ('Mr.Chocolate', 3, 'm', 'dark brown','mild',1);
