f137953b30e26d193b4e857e88e40d196d615e22
Joy Lin <vvlnote@hotmail.com>
2019-04-16 02:03:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,5 +4,5 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabith
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'm', 'brown','mild',1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 6, 'f', 'gray','mild',1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 2, 'f', 'gray','sensitivity',1);
-INSERT INTO beers (name, age, gender, color, temperament, alive) VALUES ('Grinch', 2, 'm', 'black','withdrawal',1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 2, 'm', 'black','withdrawal',1);
 INSERT INTO beers (name, age, gender, color, temperament, alive) VALUES (NULL, 3, 'm', 'black','aggressive', 0);
