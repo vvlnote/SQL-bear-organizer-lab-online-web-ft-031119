7ba79b14f9dea16253027cdccb65137a43856ee7
Joy Lin <vvlnote@hotmail.com>
2019-04-16 02:14:23 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ end
 
 def selects_oldest_bear_and_returns_name_and_age
   "Write your SQL query here"
-  "SELECT name, age FROM bears ORDER BY age DES"
+  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
