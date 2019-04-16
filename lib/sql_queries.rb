faad96ef013f6dcf6002d1464772dfc7d69cf4a5
Joy Lin <vvlnote@hotmail.com>
2019-04-16 02:19:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,7 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   "Write your SQL query here"
-  "SELECT name, age FROM bears ORDER BY age LIMIT 1;"
+  "SELECT COUNT FROM bears ORDER BY age LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
